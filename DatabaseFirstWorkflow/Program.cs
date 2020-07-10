4cca0d550bc54af3d9a3d6cb68d713de2b1d5d36
maksimovicnikola <nikola.maksimovic.jobs@gmail.com>
2020-07-10 13:38:25 +0200

    Database first approach used for connecting the app via database. Practice modifying database and table fields.

---
+++ DatabaseFirstWorkflow/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseFirstWorkflow
+{
+    class Program
+    {
+        static void Main()
+        {
+            var context = new PlutoEntities();
+            var course = new Course();
+            course.Level = CourseLevels.Begineer;
+
+            // Call procedure and print all titles
+            var courses = context.GetCourses();
+            foreach (var c in courses)
+            {
+                Console.WriteLine(c.Title);
+            }
+
+            context.GetAuthorCourses(1);
+
+            Console.Read();
+        }
+    }
+}
