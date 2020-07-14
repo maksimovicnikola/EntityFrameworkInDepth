60b238fe4e9d58830abf64de8b2f1b224427c471
maksimovicnikola <nikola.maksimovic.jobs@gmail.com>
2020-07-14 13:53:54 +0200

    Learning code first workflow implementation with the existing database.

---
+++ CodeFirstExistingDatabase/Migrations/202007140929325_AddCategoriesTable.cs
@@ -0,0 +1,29 @@
+﻿namespace CodeFirstExistingDatabase.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddCategoriesTable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+
+            //-- Withing SQL function we can run any sql expression
+            Sql("INSERT INTO CATEGORIES (Name) VALUES ('Web Development')");
+            Sql("INSERT INTO CATEGORIES (Name) VALUES ('Programming Languages')");
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Categories");
+        }
+    }
+}
---
+++ CodeFirstExistingDatabase/Migrations/202007141057108_DeleteCategoriesTable.cs
@@ -0,0 +1,42 @@
+﻿namespace CodeFirstExistingDatabase.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+
+    public partial class DeleteCategoriesTable : DbMigration
+    {
+        public override void Up()
+        {
+            //-- If we want  to keep categories table data for historical reason, we can do it on this way
+            CreateTable(
+                "dbo._Categories",
+                    c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                    .PrimaryKey(t => t.Id);
+            //-- Copy data from Categories to _Categories
+            Sql("INSERT INTO _Categories (Name) SELECT Name FROM Categories");
+
+            DropTable("dbo.Categories");
+        }
+
+        public override void Down()
+        {
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                {
+                    Id = c.Int(nullable: false, identity: true),
+                    Name = c.String(),
+                })
+                .PrimaryKey(t => t.Id);
+
+            //-- Copy data from Categories to _Categories
+            Sql("INSERT INTO Categories (Name) SELECT Name FROM _Categories");
+
+            DropTable("dbo._Categories");
+        }
+    }
+}
