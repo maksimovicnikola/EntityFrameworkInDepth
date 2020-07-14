eda0cdd5fa57ef0fe5bb1ae62b66eff60391ea90
maksimovicnikola <nikola.maksimovic.jobs@gmail.com>
2020-07-14 09:14:44 +0200

    Created a little bit more complex database with a code first workflow.

---
+++ CodeFirstWorkflow/Models/PlutoContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+
+namespace CodeFirstWorkflow
+{
+    public class PlutoContext : DbContext
+    {
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+        //-- We need this specified because we didn't call a connection string with name of created context("PlutoContext"),
+        //-- but with name="DefaultConnection", so we need to tell that to the Entity Framework
+        public PlutoContext() : base("name=DefaultConnection")
+        {
+        }
+    }
+}
